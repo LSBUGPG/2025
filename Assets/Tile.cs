7642df41c4bcd276f76be9d0bf7049112223bfe3
Paul Sinnett <paul.sinnett@gmail.com>
2025-10-20 23:45:31 +0100

    update

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -10,7 +10,6 @@ public class Tile : MonoBehaviour
     {
         dragging = true;
         offset = transform.position - world;
-        Debug.Log($"{name} was clicked at {world}");
     }
 
     void Update()
@@ -20,7 +19,6 @@ public class Tile : MonoBehaviour
             if (Mouse.current.leftButton.wasReleasedThisFrame)
             {
                 dragging = false;
-                Debug.Log($"{name} was released");
             }
             else if (Mouse.current.delta.magnitude > 0)
             {
